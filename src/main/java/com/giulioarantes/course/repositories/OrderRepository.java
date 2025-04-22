1fe9815f01e8251b559b598b2ad85b92235c0fe3
GiulioArantes <giulio.arantes@icloud.com>
2025-04-22 00:04:31 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/giulioarantes/course/repositories/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.giulioarantes.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.giulioarantes.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
