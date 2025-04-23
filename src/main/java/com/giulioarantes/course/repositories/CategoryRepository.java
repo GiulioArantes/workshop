45fc023c96d18e301b84c4c57a2204ae742d4e3a
GiulioArantes <giulio.arantes@icloud.com>
2025-04-23 16:03:38 -0300

    Category

---
+++ src/main/java/com/giulioarantes/course/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.giulioarantes.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.giulioarantes.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
